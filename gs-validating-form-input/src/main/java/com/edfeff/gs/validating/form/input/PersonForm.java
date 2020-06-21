b41838b7aaa81adca343d80d3568ddd19116e22f
edfeff <396859442@qq.com>
2020-06-22 07:25:41 +0800

    gs-validating-form-input

---
+++ gs-validating-form-input/src/main/java/com/edfeff/gs/validating/form/input/PersonForm.java
@@ -0,0 +1,42 @@
+package com.edfeff.gs.validating.form.input;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author wangpp
+ */
+public class PersonForm {
+    @NotNull
+    @Size( min = 2, max = 30 )
+    private String name;
+
+    @NotNull
+    @Min( 18 )
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonForm{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ gs-validating-form-input/src/test/java/com/edfeff/gs/validating/form/input/GsValidatingFormInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edfeff.gs.validating.form.input;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsValidatingFormInputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
